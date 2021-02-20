9591d77cff322d095ed284eae133befbb2b9b749
Edgar Hernandez <EdgarHomeFolder@edgars-mbp.attlocal.net>
2021-02-20 13:43:18 -0600

    initial commit

---
+++ src/main/java/org/launchcode/hellospringtemplate/HelloSpringTemplateApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.hellospringtemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSpringTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloSpringTemplateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/launchcode/hellospringtemplate/HelloSpringTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.hellospringtemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpringTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
