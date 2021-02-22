6226cf184ad132a02a542a7f21872d30cda20e71
Edgar Hernandez <EdgarHomeFolder@edgars-mbp.attlocal.net>
2021-02-21 19:42:02 -0600

    finished

--- src/main/java/org/launchcode/hellospringtemplate/controllers/HelloSpringController.java
+++ src/main/java/org/launchcode/hellospringtemplate/controllers/HelloSpringController.java
@@ -1,7 +1,11 @@
 package org.launchcode.hellospringtemplate.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class HelloSpringController {
@@ -21,16 +25,20 @@ public String goodbye() {
 
 // Handles requests of the form /hello?name=LaunchCode
 @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST}, value = "hello")
-@ResponseBody
-public String helloWithQueryParam(@RequestParam String name) {
-    return "Hello, " + name + "!";
+public String helloWithQueryParam(@RequestParam String name, Model model) {
+    String greeting = "Hello, " + name + "!";
+    //first attribute has to match name given to template variable
+    //second attribute has to match up with local variable
+    model.addAttribute("greeting", greeting);
+    return "hello";
 }
 
 // Handles requests of the form /hello/LaunchCode
 @GetMapping("hello/{name}")
-@ResponseBody
-public String helloWithPathParam(@PathVariable String name) {
-    return "Hello, " + name + "!";
+public String helloWithPathParam(@PathVariable String name, Model model) {
+    String greeting = "Hello, " + name + "!";
+    model.addAttribute("greeting", greeting);
+    return "hello";
 }
 
 
@@ -38,5 +46,17 @@ public String helloWithPathParam(@PathVariable String name) {
 public String helloForm() {
     return "form";
         }
+
+@GetMapping("hello-names")
+public String helloNames(Model model){
+    List<String> names = new ArrayList<>();
+    names.add("Java");
+    names.add("LaunchCode");
+    names.add("Python");
+    model.addAttribute("names", names);
+    return "hello-list";
+}
+
+
 
     }
